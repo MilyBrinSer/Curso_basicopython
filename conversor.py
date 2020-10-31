66b63c661a337599c329f02dc879a47cf6965f6c
MilyBrinSer <70125018+MilyBrinSer@users.noreply.github.com>
2020-10-31 15:13:26 -0500

    cpaitulo 3 y 4

--- conversor.py
+++ conversor.py
@@ -1,7 +1,40 @@
-pesos = input("¿Cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+menu = """
+Bienvenidos al coversor de monedas 💰
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opcion: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+
+elif opcion == 2:
+    pesos = input("¿Cuantos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 3:
+    paspesos = input("¿Cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+else:
+    print('Ingresa una opcion correcta por favor')
+
+
